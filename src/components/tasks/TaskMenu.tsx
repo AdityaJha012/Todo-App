6a8fbf7864f21a4670cd4fdcfb689229fd5615cb
Aditya Kumar <adityajha479@gmail.com>
2024-12-12 22:37:13 +0530

    task menu functionality

--- src/components/tasks/TaskMenu.tsx
+++ src/components/tasks/TaskMenu.tsx
@@ -13,7 +13,7 @@ import {
     PushPinRounded,
     RadioButtonChecked,
     RecordVoiceOver,
-    RecordVoiceOverRounded
+    RecordVoiceOverRounded,
 } from "@mui/icons-material";
 import { Divider, IconButton, Menu, MenuItem } from "@mui/material";
 import { Emoji, EmojiStyle } from "emoji-picker-react";
@@ -32,3 +32,150 @@ import { useTheme } from "@emotion/react";
 import { TaskContext } from "../../contexts/TaskContext";
 import { ColorPalette } from "../../theme/themeConfig";
 import { ShareDialog } from "./ShareDialog";
+
+const SheetHeader = styled.h3`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 6px;
+    color: ${({ theme }) => (theme.darkMode ? ColorPalette.fontLight : ColorPalette.fontDark)};
+    margin: 10px;
+    font-size: 20px;
+`;
+
+const SheetContent = styled.div`
+    color: ${({ theme }) => (theme.darkMode ? ColorPalette.fontLight : ColorPalette.fontDark)};
+    margin: 20px 10px;
+    & .MuiMenuItem-root {
+        font-size: 16px;
+        padding: 16px;
+        &::before {
+            content: "",
+            display: inline-block;
+            margin-right: 10px;
+        }
+    }
+`;
+
+const StyledMenuItem = styled(MenuItem)<{ clr?: string }>`
+    margin: 0 6px;
+    padding: 12px;
+    border-radius: 12px;
+    box-shadow: none;
+    gap: 2px;
+    color: ${({ clr }) => clr || "unset"};
+`;
+
+const ReadAloudContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const ReadAloudHeader = styled.div`
+    display: inline-flex;
+    align-items: center;
+    font-weight: 600;
+    gap: 6px;
+`;
+
+const ReadAloudControls = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 16px;
+    gap: 8px;
+`;
+
+export const TaskMenu = () => {
+    const { user, setUser } = useContext(UserContext);
+    const { tasks, settings, emojisStyle } = user;
+    const {
+        selectedTaskId,
+        anchorEl,
+        anchorPosition,
+        multipleSelectedTasks,
+        handleSelectTask,
+        setEditModalOpen,
+        handleDeleteTask,
+        handleCloseMoreMenu
+    } = useContext(TaskContext);
+    const [showShareDialog, setShowShareDialog] = useState<boolean>(false);
+
+    const isMobile = useResponsiveDisplay();
+    const n = useNavigate();
+    const theme = useTheme();
+
+    const selectedTask = useMemo(() => {
+        return tasks.find((task) => task.id === selectedTaskId) || ({} as Task);
+    }, [selectedTaskId, tasks]);
+
+    const redirectToTaskDetails = () => {
+        const taskId = selectedTask?.id.toString().replace(".", "");
+        n(`/task/${taskId}`);
+    };
+
+    const handleMarkAsDone = () => {
+        // Toggles the "done" property of the selected task
+        if (selectedTaskId) {
+            handleCloseMoreMenu();
+
+            const updatedTasks = tasks.map((task) => {
+                if (task.id === selectedTaskId) {
+                    return { ...task, done: !task.done };
+                }
+
+                return task;
+            });
+
+            setUser((prevUser) => ({
+                ...prevUser,
+                tasks: updatedTasks
+            }));
+
+            const allTasksDone = updatedTasks.every((task) => task.done);
+
+            if (allTasksDone) {
+                showToast(
+                    <div>
+                        <b>All tasks done</b>
+                        <br />
+                        <span>You've checked off all your todos. Well done!</span>
+                    </div>,
+                    {
+                        icon: (
+                            <div style={{ margin: "-6px 4px -6px -6px" }}>
+                                <TaskIcon variant="success" scale={0.18} />
+                            </div>
+                        ),
+                    },
+                );
+            }
+        }
+    };
+
+    const handlePin = () => {
+        // Toggles the "pinned" property of the selected task
+        if (selectedTaskId) {
+            handleCloseMoreMenu();
+
+            const updatedTasks = tasks.map((task) => {
+                if (task.id === selectedTaskId) {
+                    return { ...task, pinned: !task.pinned };
+                }
+
+                return task;
+            });
+
+            setUser((prevUser) => ({
+                ...prevUser,
+                tasks: updatedTasks
+            }));
+        }
+    };
+
+    const handleDuplicateTask = () => {
+        
+    };
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,4 @@ export { systemInfo } from './getSystemInfo';
 export { showToast } from './showToast';
 export { generateUUID } from './generateUUID';
 export { timeAgo, formatDate, calculateDateDifference } from './timeUtils';
+export { saveQRCode } from "./saveQRCode";
